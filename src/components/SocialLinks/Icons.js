a810cfcb64add39d66de3ac3f80f6d50640d1f41
Igor Tuag <igortuag@gmail.com>
2020-11-08 17:49:28 -0300

    SocialLinks content and Icons

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,13 @@
+import { GitHub } from "@styled-icons/boxicons-logos/Github"
+import { Twitter } from "@styled-icons/boxicons-logos/Twitter"
+import { LinkedinSquare } from "@styled-icons/boxicons-logos/LinkedinSquare"
+import { Instagram } from "@styled-icons/boxicons-logos/Instagram"
+
+const Icons = {
+  Github,
+  Twitter,
+  LinkedinSquare,
+  Instagram,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,22 @@
+const handle = "igortuag"
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/${handle}`,
+  },
+  {
+    label: "LinkedIn",
+    url: `https://linkedin.com/in/${handle}`,
+  },
+  {
+    label: "Instagram",
+    url: `http://instagram.com/${handle}`,
+  },
+]
+
+export default links
