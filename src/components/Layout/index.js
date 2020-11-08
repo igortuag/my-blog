2c58636fda2da83a728eeda8fa1d2c1e323dfcfd
Igor Tuag <igortuag@gmail.com>
2020-11-08 16:33:58 -0300

    add global styles

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,14 +2,15 @@ import React from "react"
 import PropTypes from "prop-types"
 import Profile from "../Profile"
 
-import styled from 'styled-components'
+import styled from "styled-components"
+import GlobalStyles from "../../styles/global"
 
 const LayoutWrapper = styled.section`
   display: flex;
 `
 
 const LayoutMain = styled.main`
-  background: #16202;
+  background: #16202c;
   min-height: 100vh;
   padding: 0 3.75rem 0 20rem;
   width: 100%;
@@ -18,6 +19,7 @@ const LayoutMain = styled.main`
 const Layout = ({ children }) => {
   return (
     <LayoutWrapper>
+      <GlobalStyles />
       <Profile />
       <aside></aside>
       <LayoutMain>{children}</LayoutMain>
