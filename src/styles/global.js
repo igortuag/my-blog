7c1524ebf809299f0d8a5f0e0b8d8761c70fbd97
Igor Tuag <igortuag@gmail.com>
2020-11-10 19:33:33 -0300

    add styles to blog post

--- src/styles/global.js
+++ src/styles/global.js
@@ -56,7 +56,7 @@ body {
 	background: #16202c;
 	line-height: 1;
 	font-size: 100%;
-	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif,
+	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 img {
 	display: block;
