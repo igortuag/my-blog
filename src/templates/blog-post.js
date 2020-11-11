94f9b1513d37839b1ea76a2a1bae89831f0187d0
Igor Tuag <igortuag@gmail.com>
2020-11-10 21:06:17 -0300

    Add recommendedPosts and Comments

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,15 +3,18 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import RecommendedPosts from '../components/RecommendedPosts'
 
 import * as S from "../components/Post/styled"
 
-const BlogPost = ({ data }) => {
+const BlogPost = ({ data, pageContext }) => {
   const post = data.markdownRemark
+  const next = pageContext.nextPost
+  const previous = pageContext.previousPost
 
   return (
     <Layout>
-      <SEO title={post} />
+      <SEO title={post.frontmatter.title} />
       <S.PostHeader>
         <S.PostDate>
           {post.frontmatter.date} • {post.timeToRead} min de leitura
@@ -22,6 +25,7 @@ const BlogPost = ({ data }) => {
       <S.MainContent>
         <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
       </S.MainContent>
+      <RecommendedPosts next={next} previous={previous} />
     </Layout>
   )
 }
