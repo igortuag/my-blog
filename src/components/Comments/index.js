8dd54c1810bfe53db2582d056c369d1e7990740f
Igor Tuag <igortuag@gmail.com>
2020-11-13 20:25:03 -0300

    Configure links

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -5,7 +5,7 @@ import ReactDisqusComments from "react-disqus-comments"
 import * as S from "./styled"
 
 const Comments = ({ url, title }) => {
-  const completeURL = `https://igortuag.com${url}`
+  const completeURL = `https://igortuag.netlify.app${url}`
 
   return (
     <S.CommentsWrapper>
